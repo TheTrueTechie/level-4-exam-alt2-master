20d685b58b37d4c4a8bf838686b610c682c6d56a
TheTrueTechie <Shiva.malhotra.email@gmail.com>
2017-08-31 18:23:30 -0700

    Update!

--- src/CountingDuplicates.java
+++ src/CountingDuplicates.java
@@ -2,26 +2,27 @@ import java.util.Scanner;
 
 public class CountingDuplicates {
 	public static int duplicateCount(String word) {
-		char letter; 
-		int lengthOfWord = 0; 
-		int amountOfRepeatedWords = 0; 
-		lengthOfWord = word.length(); 
+		char letter;
+		int lengthOfWord = 0;
+		int amountOfRepeatedWords = 0;
+		lengthOfWord = word.length();
 		for (int i = 0; i < word.length(); i++) {
 			letter = word.charAt(i);
-			if (letter==word.charAt(i) || letter==word.charAt(i+1)) {
-				amountOfRepeatedWords++; 
-				
+			if (letter == word.charAt(i) || letter == word.charAt(i + 1) || letter == word.charAt(i + 2)) {
+				amountOfRepeatedWords++;
+
 			}
 		}
 		return amountOfRepeatedWords;
 	}
-	
+
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
 			while (true) {
 				final String nextLine = scanner.nextLine();
-				if (nextLine.trim().isEmpty()) break;
-				
+				if (nextLine.trim().isEmpty())
+					break;
+
 				System.out.println(duplicateCount(nextLine));
 			}
 		}
